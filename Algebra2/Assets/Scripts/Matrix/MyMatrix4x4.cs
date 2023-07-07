b03fbca3cb7268d4f1b2969fd73caeb79009e6d0
BarraSantiago <56093356+BarraSantiago@users.noreply.github.com>
2023-07-06 23:52:14 -0300

    Started Matrix4x4, added variables, constructor, zero and identity

--- Algebra2/Assets/Scripts/Matrix/MyMatrix4x4.cs
+++ Algebra2/Assets/Scripts/Matrix/MyMatrix4x4.cs
@@ -1,17 +1,75 @@
-﻿using CustomMath;
-using UnityEngine;
+﻿using System;
 
 namespace Matrix
 {
     public class MyMatrix4x4
     {
+        #region Variables
+
+        public float[,] matrix;
+        public float a1;
+        public float a2;
+        public float a3;
+        public float a4;
+        public float b1;
+        public float b2;
+        public float b3;
+        public float b4;
+        public float c1;
+        public float c2;
+        public float c3;
+        public float c4;
+        public float d1;
+        public float d2;
+        public float d3;
+        public float d4;
+
+        #endregion
+
+        public MyMatrix4x4()
+        {
+            matrix = new float[4, 4];
+        }
+
+        public MyMatrix4x4(float[,] data)
+        {
+            if (data.GetLength(0) != 4 || data.GetLength(1) != 4)
+            {
+                throw new ArgumentException("Matrix dimensions are invalid. Must be 4x4.");
+            }
+
+            matrix = data;
+        }
+
+        public MyMatrix4x4(float a1, float a2, float a3, float a4,
+            float b1, float b2, float b3, float b4,
+            float c1, float c2, float c3, float c4,
+            float d1, float d2, float d3, float d4)
+        {
+            this.a1 = a1;
+            this.a2 = a2;
+            this.a3 = a3;
+            this.a4 = a4;
+            this.b1 = b1;
+            this.b2 = b2;
+            this.b3 = b3;
+            this.b4 = b4;
+            this.c1 = c1;
+            this.c2 = c2;
+            this.c3 = c3;
+            this.c4 = c4;
+            this.d1 = d1;
+            this.d2 = d2;
+            this.d3 = d3;
+            this.d4 = d4;
+        }
+
+        public static MyMatrix4x4 zero = new MyMatrix4x4(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+        public static MyMatrix4x4 identity = new MyMatrix4x4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
         /*
-        variables
-            constructor
         this[int index]
         this[int row, int column]
-        zero
-            identity
         rotation
             lossyScale
         isIdentity
